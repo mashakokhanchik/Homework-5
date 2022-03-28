94f3206936b4eee98acb6b727ff60521075dfc96
Masha Kokhanchik <masha.kokhanchik@gmail.com>
2022-03-28 08:50:16 +0300

    Create project

---
+++ Homework-5.2/Homework-5.2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Homework-5.2
+//
+//  Created by Мария Коханчик on 28.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
